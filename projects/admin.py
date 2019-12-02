7b3653e6559e2ad1e184e376a2a604aac5d67706
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2019-12-02 19:13:56 +0200

    UX подобрения

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Project, LegalEntity, User, MoneySupport, TimeSupport
+from .models import Project, LegalEntity, User, MoneySupport, TimeSupport, Announcement
 from vote.models import Vote
 
 # Register your models here.
@@ -12,3 +12,4 @@ admin.site.register(User, UserAdmin)
 admin.site.register(Vote)
 admin.site.register(MoneySupport)
 admin.site.register(TimeSupport)
+admin.site.register(Announcement)
