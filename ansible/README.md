d40d37583e81c402907ef1487a88d93762e3a81f
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2020-01-02 09:42:35 +0200

    Ansible настройки за автоматично вдигане на работеща Хородея Django, uwsgi и nginx върху хост

---
+++ ansible/README.md
@@ -0,0 +1,1 @@
+ansible-playbook -i production playbook.yaml --private-key=secret/id_rsa_horodeya.pem --vault-password=get_pass.sh
---
+++ ansible/roles/horodeya/files/manage.sh
@@ -0,0 +1,13 @@
+export DB_HOST='localhost'
+export SECRET_KEY='{{secret_key}}'
+export AWS_ACCESS_KEY_ID='{{aws_access_key_id}}'
+export AWS_SECRET_ACCESS_KEY='{{aws_secret_access_key}}'
+export AWS_DEFAULT_REGION='eu-west-1'
+export ANYMAIL_WEBHOOK_SECRET='{{anymail_webhook_secret}}'
+
+export STREAM_API_KEY='{{stream_api_key}}'
+export STREAM_API_SECRET='{{stream_api_secret}}'
+export DB_PASSWORD='{{db_password}}'
+export DB_NAME='{{db_name}}'
+
+venv/bin/python manage.py $@
