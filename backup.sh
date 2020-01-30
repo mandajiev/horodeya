1276523016954deb7ab3a0553093fae4640dc822
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2020-01-30 11:34:02 +0200

    Подобрен backup скрипт

--- backup.sh
+++ backup.sh
@@ -1,3 +1,5 @@
-TIMESTAMP=`date +%Y-%m-%d_%H:%M:%S`
-mkdir -p backup
-./manage.sh dumpdata --natural-foreign --format yaml -o backup/$TIMESTAMP.yaml -e auth.Permission -e sessions -e admin.logentry --exclude contenttypes
+#!/bin/bash
+
+TIMESTAMP=`/bin/date +%Y-%m-%d_%H:%M:%S`
+/bin/mkdir -p backup
+bash manage.sh dumpdata --natural-foreign --format yaml -o backup/$TIMESTAMP.yaml -e auth.Permission -e sessions -e admin.logentry --exclude contenttypes
