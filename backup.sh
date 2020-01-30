5e6dc52ba8b32203447627593a09038460128c34
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2020-01-30 11:36:52 +0200

    Още подобрен бакъп скрипт

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
 TIMESTAMP=`/bin/date +%Y-%m-%d_%H:%M:%S`
 /bin/mkdir -p backup
 bash manage.sh dumpdata --natural-foreign --format yaml -o backup/$TIMESTAMP.yaml -e auth.Permission -e sessions -e admin.logentry --exclude contenttypes
